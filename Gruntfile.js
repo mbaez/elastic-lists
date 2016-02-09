be457ce2460522a325b8bd1b8b9041b33cfee013
Maximiliano Báez González <mxbg.py@gmail.com>
2016-02-09 13:32:04 -0300

    Upadating examples

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,82 @@
 module.exports = function (grunt) {
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded',
-                    sourcemap: false
-                },
-                files: {
-                    'test/css/main.css': 'test/scss/main.scss',
-                    'elastic-list.css' :'elastic-list.scss'
-                }
-            }
-        },
-        copy: {
-            main: {
-                files: [{
-                    cwd: 'test/vendors/bootstrap-sass/assets/fonts',
-                    src: '**/*', // copy all files and subfolders
-                    dest: 'test/fonts', // destination folder
-                    expand: true // required when using cwd
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded',
+					sourcemap: false
+				},
+				files: {
+					'dist/css/main.css': 'test/scss/main.scss',
+					'dist/elastic-list.css': 'elastic-list.scss'
+				}
+			}
+		},
+
+		copy: {
+			main: {
+				files: [{
+					cwd: 'test/vendors/bootstrap-sass/assets/fonts',
+					src: '**/*', // copy all files and subfolders
+					dest: 'dist/fonts', // destination folder
+					expand: true // required when using cwd
+                }, {
+					cwd: 'test/',
+					src: '**/*', // copy all files and subfolders
+					dest: 'dist/', // destination folder
+					expand: true // required when using cwd
                 }]
-            }
-        }
-    });
-    // Default task.
-    grunt.registerTask('default', [ 'sass', 'copy']);
+			}
+		},
+
+		connect: {
+			server: {
+				options: {
+					hostname: '0.0.0.0',
+					port: 8888,
+					base: "dist",
+					livereload: true
+				}
+			}
+		},
+
+		open: {
+			all: {
+				path: 'http://localhost:8888'
+			}
+		},
+
+		uglify: {
+			options: {
+				mangle: false,
+				sourceMap: false
+			},
+			build: {
+				files: [{
+					expand: true,
+					src: '*.js',
+					dest: 'dist',
+					ext: '.js',
+					extDot: 'last'
+                }]
+			}
+		},
+
+		watch: {
+			main: {
+				options: {
+					livereload: true
+				},
+				files: ['test/**/*', '*.js'],
+				tasks: ['default']
+			}
+		},
+	});
+
+	// Default task.
+	grunt.registerTask('default', ['sass', 'uglify', 'copy']);
+	grunt.registerTask('serve', ['default', "open", 'connect:server', 'watch']);
 };
--- test/count-column.js
+++ test/count-column.js
@@ -1,28 +1,29 @@
 $(function () {
-    view = new ElasticList({
-        el: $("#main"),
-        data: dataCount,
-        onchange: function (filters) {
-            console.info(filters);
-        },
-        hasFilter: true,
-        countColumn: "total",
-        columns: [
-            {
-                title: "Presentacion",
-                attr: "csv_presentacion",
-                formatter: function (value) {
-                    if (value == "UNI") {
-                        return "Uni formater value";
-                    }
-                    return value;
-                }
+	view = new ElasticList({
+		el: $("#main"),
+		data: dataCount,
+		onchange: function (filters) {
+			console.info(filters);
+		},
+		hasFilter: true,
+		countColumn: "total",
+		align: 'horizontal',
+		columns: [
+			{
+				title: "Presentacion",
+				attr: "csv_presentacion",
+				formatter: function (value) {
+					if (value == "UNI") {
+						return "Uni formater value";
+					}
+					return value;
+				}
             }, {
-                title: "Marca",
-                attr: "csv_marca"
+				title: "Marca",
+				attr: "csv_marca"
             }, {
-                title: "Modalidad",
-                attr: "csv_modalidad"
+				title: "Modalidad",
+				attr: "csv_modalidad"
             }]
-    });
+	});
 });
